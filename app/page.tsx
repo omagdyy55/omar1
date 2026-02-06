73ba293b8bab840da0527c331331e72a5a453225
omagdyy55 <omagdyy55@gmail.com>
2026-02-06 23:21:09 +0200

    hel

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@
     className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
     href="/login"
   >
-    Login
+    Go to Login
   </a>
 </div>
